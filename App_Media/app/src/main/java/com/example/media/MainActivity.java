9cbd5503bdb0d3218e8638315e4239b57e5ce3b1
Pm-Tech-IT-Template <111478523+Pm-Tech-IT-Template@users.noreply.github.com>
2022-09-08 09:42:43 +0700

    Work_Media

---
+++ App_Media/app/src/main/java/com/example/media/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.media;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mySound;
+    Button ms1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mySound=MediaPlayer.create(this,R.raw.sound1);
+        System.out.println(mySound);
+
+        mySound.start();
+        mySound.setLooping(true);
+
+        ms1 = (Button) findViewById(R.id.btnnext);
+        ms1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ms1 = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(ms1);
+                mySound.stop();
+            }
+        });
+    }
+}
